cebcfeef59e3e61b5b26a9c9154467e1032057cb
Maria Valdivieso <134414585+mariav0721@users.noreply.github.com>
2023-09-21 17:25:47 -0700

    Third Commit

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,69 +1,115 @@
 // Store our API endpoint as queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(earthquakeData) {
+  
+      console.log(earthquakeData);
+      
+      createFeatures(earthquakeData.features);
+  });
 
-// Perform a GET request to the query URL.
-d3.json(queryUrl).then(function (data) {
 
-  console.log(data.features);
+// Create markers whose size increases with magnitude and color with depth
+function createMarker(feature, latlng) {
+  return L.circleMarker(latlng, {
+      radius: markerSize(feature.properties.mag),
+      fillColor: markerColor(feature.geometry.coordinates[2]),
+      color:"#000",
+      weight: 0.5,
+      opacity: 0.5,
+      fillOpacity: 1
+  });
+}
 
-  // Using the features array sent back in the API data, create a GeoJSON layer, and add it to the map.
-
-  function useFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+function createFeatures(earthquakeData) {
+ 
+  function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place}<h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`);
   }
 
-  let earthquakes = L.geoJSON(data.features, {
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature,
+      pointToLayer: createMarker
+  });
+  
 
-    
-
-
-//This creates the circles
-    onEachFeature: useFeature,
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng);
-}
-})
-
+  // Send earthquakes layer to the createMap function
   createMap(earthquakes);
-});
-
+}
 
 function createMap(earthquakes) {
-  // Create the base layers.
+  // Create the base layers
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
+    });
 
   let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
 
-  // Create a baseMaps object.
+  // Create a baseMaps object
   let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
+      "Street Map": street,
+      "Topographic Map": topo
   };
 
-  // Create an overlays object.
+  // Create an overlay object to hold our overlay.
   let overlayMaps = {
-    Earthquakes: earthquakes,
+      "Earthquakes": earthquakes,
+      
   };
-  
-  // Create a new map.
-  // Edit the code to add the earthquake data to the layers.
+
+  // Create map
   let myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
+      center: [37.09, -95.71],
+      zoom: 5,
+      layers: [street, earthquakes]
   });
 
-  // Create a layer control that contains our baseMaps.
-  // Be sure to add an overlay Layer that contains the earthquake GeoJSON.
+  // Create a control
+  // Pass in baseMaps and overlayMaps
+  // Add the control to the map
   L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+      collapsed: false
+  }).addTo(myMap); 
+  
+  let legend = L.control({position: 'bottomright'});
 
+  legend.onAdd = function (myMap) {
+
+      let div = L.DomUtil.create('div', 'info legend'),
+          grades = [-10, 10, 30, 60, 90],
+          labels = [],
+          legendInfo = "<h5>Magnitude</h5>";
+
+      for (let i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + markerColor(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }    
+
+      return div;
+
+      };
+
+      // Add legend to map
+      legend.addTo(myMap);
+}
+
+// Increase marker size based on magnitude
+function markerSize(magnitude) {
+  return magnitude * 5;
+}
+
+// Change marker color based on depth
+function markerColor(depth) {
+  return depth > 90 ? '#d73027' :
+          depth > 70 ? '#fc8d59' :
+          depth > 50 ? '#fee08b' :
+          depth > 30 ? '#d9ef8b' :
+          depth > 10 ? '#91cf60' :
+                       '#1a9850' ;          
 }
